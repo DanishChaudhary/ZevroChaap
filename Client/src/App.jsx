4ba0161c94be49216809809fe03b3196c324c7fa
Danish Chaudhary <danishvipforbca@gmail.com>
2025-09-17 11:38:38 +0530

    ZevroChaap

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
 import Franchise from './pages/Franchise';
@@ -10,22 +10,31 @@ import Footer from './components/layout/Footer';
 import WhatsAppButton from './components/WhatsAppButton';
 import ScrollToTop from './components/ScrollToTop';
 
+function AppContent() {
+  const location = useLocation();
+  const isAdminPage = location.pathname === '/admin';
+
+  return (
+    <div className="App">
+      {!isAdminPage && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/franchise" element={<Franchise />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+      {!isAdminPage && <Footer />}
+      {!isAdminPage && <WhatsAppButton />}
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
       <ScrollToTop />
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/franchise" element={<Franchise />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/menu" element={<Menu />} />
-        </Routes>
-        <Footer />
-        <Route path="/admin" element={<Admin />} />
-        <WhatsAppButton />
-      </div>
+      <AppContent />
     </Router>
   );
 }
